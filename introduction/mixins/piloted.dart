b2a9852c59e7c710a9eb9abe7631884793639dbd
Akira Kanno <teinen.github@gmail.com>
2019-08-12 17:10:41 +0900

    Add mixins

---
+++ introduction/mixins/piloted.dart
@@ -0,0 +1,6 @@
+class Piloted {
+  int astronauts = 1;
+  void describeCrew() {
+    print('Number of astronauts: $astronauts');
+  }
+}
---
+++ introduction/mixins/pilotedSpacecraft.dart
@@ -0,0 +1,8 @@
+import '../classes/spacecraft.dart';
+import 'piloted.dart';
+
+class PilotedSpacecraft extends Spacecraft with Piloted {
+  PilotedSpacecraft(String name, DateTime launchDate) : super(name, launchDate);
+
+  // Some methods...
+}
