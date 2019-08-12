b28f765ee919e241221bf69df8bf19c117f849c5
Akira Kanno <teinen.github@gmail.com>
2019-08-12 13:24:11 +0900

    Add inheritance

---
+++ introduction/inheritance/inheritance.dart
@@ -0,0 +1,6 @@
+import 'orbiter.dart';
+
+void main() {
+  var orbiter = Orbiter('Endeavour orbiter', DateTime(1992, 5, 7), 960);
+  orbiter.describe();
+}
---
+++ introduction/inheritance/orbiter.dart
@@ -0,0 +1,8 @@
+import '../classes/spacecraft.dart';
+
+class Orbiter extends Spacecraft {
+  num altitude;
+
+  Orbiter(String name, DateTime launchDate, this.altitude)
+      : super(name, launchDate);
+}
